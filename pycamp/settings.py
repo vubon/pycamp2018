b69ee97ba4deca8549d22c3727747f524e23b786
vubon <vubon.roy@gmail.com>
2018-05-04 18:04:10 +0600

    do not push your code in master

--- pycamp/settings.py
+++ pycamp/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'pycamp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'NAME': 'pycamp',
+        'USER': 'postgres',
+        'PASSWORD': 'vubon',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

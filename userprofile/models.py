0772a651dc1b860f05faad54d18547b2a050577e
faysalmehedi <fmehedi1992@gmail.com>
2018-05-05 10:22:24 +0600

    modified models.py

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,14 +3,16 @@ from django.contrib.postgres.fields import JSONField
 
 
 class UserProfileBasic(models.Model):
-	name = models.CharField(max_length=100)
-	email = models.EmailField(max_length=100)
+	auth = models.OneToOneField(User, on_delete=models.CASCADE)
 	contact = models.CharField(max_length=11)
 	current_address = models.TextField(max_length=500)
 	date_joined = models.DateTimeField(auto_now_add=True)
 	status = models.BooleanField(default=True)
 	gravatar = models.CharField(max_length=100)
 	guid = models.CharField(max_length=40)
+
+	class Meta:
+		abstract = True
 
 	def __str__(self):
 		return self.name
@@ -29,8 +31,7 @@ Gender_status = (
 
 )
 
-class PersonalProfile(models.Model):
-	user_profile = models.OneToOneField(UserProfileBasic, null=True, on_delete=models.CASCADE)
+class PersonalProfile(UserProfileBasic):
 	about = models.TextField(blank=True, null=True)
 	date_of_birth = models.DateField()
 	gender = models.CharField(max_length=10, choices=Gender_status, null=True, blank=True)
@@ -40,15 +41,15 @@ class PersonalProfile(models.Model):
 	experience = models.CharField(max_length=20, choices=Experience, null=True, blank=True)
 	permanent_address = models.TextField(max_length=1000)
 	interest = JSONField()
-	current_organization = models.TextField(max_length=200)
+	current_organization = models.TextField(max_length=100)
 	official_contact = models.TextField()
 	reference = models.TextField()
 
 	def __str__(self):
 		return self.name
 
-class OrganizationProfile(models.Model):
-	user_profile = models.OneToOneField(UserProfileBasic, null=True, on_delete=models.CASCADE)
+class OrganizationProfile(UserProfileBasic):
+	organization_name = models.CharField(max_length=100)
 	is_training_instute = models.BooleanField(default=False)
 	industry_type = JSONField() # I didn't write Taxonomy Model yet. So, just for now I write jsonfield. Later will add taxonomy Model
 	website = models.URLField(max_length=20)

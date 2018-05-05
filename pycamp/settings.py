94be3f398674ecb7bb07af2d9fef1e09f485b4c7
faysalmehedi <fmehedi1992@gmail.com>
2018-05-05 13:02:13 +0600

    add postgres settings

--- pycamp/settings.py
+++ pycamp/settings.py
@@ -84,22 +84,22 @@ WSGI_APPLICATION = 'pycamp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'pycamp',
-#         'USER': 'postgres',
-#         'PASSWORD': 'vubon',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pycamp2018',
+        'USER': 'faysal',
+        'PASSWORD': 'pycamp2018',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators

dcd454af04742006b7aedbea36951d1cec47d56a
eshafik <shafik.te@gmail.com>
2018-05-09 07:46:45 +0600

    settings.py modified

--- pycamp/settings.py
+++ pycamp/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pycamp2018',
-        'USER': 'faysal',
-        'PASSWORD': 'pycamp2018',
+        'USER': 'shafik',
+        'PASSWORD': 'shafik',
         'HOST': 'localhost',
         'PORT': '5432',
     }

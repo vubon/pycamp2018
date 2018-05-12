4f71a52dc38f4328234f2076347632c0c7dd7829
vubon <vubon.roy@gmail.com>
2018-05-12 12:58:39 +0600

    fiex

--- jobpost/models.py
+++ jobpost/models.py
@@ -25,7 +25,7 @@ class JobPostDetails(models.Model):
 
 
 class JobApplicant(models.Model):
-    job_id = job_id = models.ForeignKey(JobPostBasic,on_delete=models.CASCADE)
+    job_id = models.ForeignKey(JobPostBasic,on_delete=models.CASCADE)
     applicant_id = models.ForeignKey(PersonalProfile,on_delete=models.CASCADE)
     selection_confirmation= models.BooleanField(default=False)
     call_for_interview = models.BooleanField(default=False)

119f79e3338946968811659d7ef03888f0195a2c
Ikramul Alam <ikram.tanjib@gmail.com>
2018-05-18 13:54:19 +0600

    migrations file updated 2

--- jobpost/migrations/0001_initial.py
+++ jobpost/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('selection_confirmation', models.BooleanField(default=False)),
                 ('call_for_interview', models.BooleanField(default=False)),
-                ('applicant_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='userprofile.PersonalProfile')),
+                ('applicant', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='userprofile.PersonalProfile')),
             ],
         ),
         migrations.CreateModel(
